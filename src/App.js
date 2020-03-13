ddc5618b323fe3a1229eeeb9079f4d9cee88d1e7
asd313751243 <asd313751243@gmail.com>
2020-03-13 13:05:01 -0400

    Agregar el input

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import Button from './Components/Button';
 import ButtonOp from './Components/ButtonOp';
 import ButtonEq from './Components/ButtonEq';
+import Input from './Components/Input'
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      input:""
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <div className="calc-wrapper">
+            <Input input={this.state.input}></Input>
           <div className = "row">
             <Button>7</Button>
             <Button>8</Button>
